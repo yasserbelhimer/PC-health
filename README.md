1dec06af78bbaea8ff98293241c22a9b0bb8167d
yasserbelhimer <yasserbelhimer@gmail.com>
2020-09-07 20:26:09 +0100

    add check memory feature

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # PC-health
-check the health of PC
+PC-health is a script that you can use to check the health of your PC (CPU, memory, disks, network, sensors).
+
+## Setup
+Firt at all you need to install psutil and shutil libraries.
+``` bash
+pip install psutil shutil
+```
+## How it works
+Just run the following command in your working directory.
+``` bash
+python check_disk.py
+```
+
+### Tasks
+This script can be better by adding new features and good ux console.
+
+
+1 ) Add a menu where you can choose what you want to check.
+2 ) Add check network features.
+3 ) Add check sensors features.
+4 ) Any idea is welcomed.
+
+### Ressources
+psutil
+shutil
--- check_disk.py
+++ check_disk.py
@@ -6,6 +6,13 @@ print("\nCPU : " + str(psutil.cpu_percent(0.1)) + " %")
 
 disk = shutil.disk_usage(os.path.expanduser("~"))
 print("\nDisk Usage :\n")
-print("TOTAL : {} GB.".format(disk.total//2**30))
-print("USED  : {} GB.".format(disk.used//2**30))
-print("FREE  : {} GB.".format(disk.free//2**30))
+print("TOTAL : {:.2f} GB.".format(disk.total/2**30))
+print("USED  : {:.2f} GB.".format(disk.used/2**30))
+print("FREE  : {:.2f} GB.".format(disk.free/2**30))
+
+memory = psutil.virtual_memory()
+print("\nMemory Usage :\n")
+
+print("TOTAL : {:.2f} GB.".format(memory.total/2**30))
+print("USED  : {:.2f} GB.".format(memory.used/2**30))
+print("FREE  : {:.2f} GB.".format(memory.free/2**30))
